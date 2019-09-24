038a597b8e8d6dc2f4b0407efe7edb3e5e316f3c
sennoun <ysennoun@xebia.fr>
2019-09-24 20:30:36 +0200

    [ADD][dev] update readme and way to get kubernetes template yaml files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# smart-agriculture-with-k8s
+# Project `smart-agriculture-with-k8s`
 
-Build a smart agriculture project with Kubernetes
+Build a smart agriculture project with Kubernetes and Knative
 
 ## How to begin ?
 
@@ -9,10 +9,16 @@ First you have to:
 - Create a [GCP account](https://console.cloud.google.com/)
 - Create a gcp project by given a project-id (example: my-iot-project)
 - Install [Google Cloud SDK](https://cloud.google.com/sdk/docs/downloads-interactive)
+- Install `envsubst`
+    - MacOS: brew install gettext && brew link --force gettext
+    - Ubuntu: apt-get -y install gettext-devel 
+    - Centos: yum install gettext-devel
 - Install [Helm](https://helm.sh/docs/using_helm/#installing-helm), the package manager for Kubernetes
 - Install [Docker](https://docs.docker.com/install/)
+- Configure Docker for a specific repository
+    - Here we used GCP Container Repository: `gcloud auth configure-docker`
 
-## Configuration for Kubernetes cluster
+## Configure IoT platform 
 
 ### Configure gcloud
 
@@ -21,47 +27,29 @@ Modify the following parameters before those commands:
     export PROJECT_ID="your-project-id"
     export COMPUTE_ZONE="your-selected-zone"
     export COMPUTE_REGION="your-selected-region"
+    export CONTAINER_REPOSITORY="your docker repository" (for instance eu.gcr.io)
+    export PROJECT_NAME="your project name on gcp" (for instance my-iot-platform)
     gcloud config set project ${PROJECT_ID}
     gcloud config set compute/zone ${COMPUTE_ZONE}
     gcloud config set compute/region ${COMPUTE_REGION}
     gcloud components update
 
-### Configuration for Kafka deployment on Kubernetes
 
-Download following project from Kafka Confluent:
+### Run unit tests for IoT Platform
 
-    wget https://platform-ops-bin.s3-us-west-1.amazonaws.com/operator/confluent-operator-20190726-v0.65.0.tar.gz
-    tar -xvzf confluent-operator-20190726-v0.65.0.tar.gz
-    rm -f confluent-operator-20190726-v0.65.0.tar.gz
+Run the following script to run all unit tests:
 
-Make the following configuration changes:
-- Go to the helm/providers
-- Open the gcp.yaml file
-- Validate or change your region and zone or zones (if your cluster spans multiple availability zones)
-- Validate or change your storage provisioner. See Storage Classe Provisioners for configuration examples. The example below uses GCE persistent disk storage (gce-pd) and solid-state drives (pd-ssd).
-- Change the domain name for 'loadBalancer'
-- The deployment steps use SASL/PLAIN security with TLS disabled. See [Configuring security](https://docs.confluent.io/current/installation/operator/co-security.html#co-security)  to set up the component YAML files with TLS enabled
-
+    ./deploy/iot-handler.sh test-unit
 
 ### Install IoT Platform
 
 Run the following script to install this IoT platform on your GCP Account:
 
-    ./infrastructure/iot_platform.sh install
+    ./deploy/iot-handler.sh deploy-all
     
-
 ### Delete IoT Platform
 
 Run the following script to delete the IoT platform on your GCP Account:
 
-    ./infrastructure/iot_platform.sh delete
-    
-    
-    
-### Container Registry
-
-    gcloud auth configure-docker
-    docker build -f <dockerfile> -t <image:version> .
-    docker tag <image:version> eu.gcr.io/ysennoun-iot/<image:version>
-    docker tag iot-kn-function eu.gcr.io/ysennoun-iot/iot-kn-function
+    ./deploy/iot-handler.sh delete-all
 
--- deploy/configuration/configuration.sh
+++ deploy/configuration/configuration.sh
@@ -10,7 +10,7 @@ CONNECTOR_IMAGE="iot-mqtt-client"
 NOTIFICATION_IMAGE="iot-mqtt-client"
 CONNECTOR_IMAGE="iot-mqtt-client"
 VERSION="latest"
-REPOSITORY=${REPOSITORY}
+CONTAINER_REPOSITORY=${CONTAINER_REPOSITORY}
 PROJECT_NAME=${PROJECT_NAME}
 
 function deploy_api_image(){
@@ -18,8 +18,8 @@ function deploy_api_image(){
     do
         API_IMAGE="iot-api-"${STORAGES[$ix]}
         docker build -f ${BASE_PATH}/deploy/configuration/api/docker-images/Dockerfile-${API_IMAGE} -t ${API_IMAGE}:${VERSION} .
-        docker tag ${API_IMAGE}:${VERSION} ${REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
-        docker push ${REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
+        docker tag ${API_IMAGE}:${VERSION} ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
+        docker push ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
     done
 }
 
@@ -27,7 +27,8 @@ function deploy_api_application(){
     for ix in ${!STORAGES[*]}
     do
         API_IMAGE="iot-api-"${STORAGES[$ix]}
-        kubectl apply -f ${BASE_PATH}/deploy/configuration/api/${API_IMAGE}.yaml
+        template=$(envsubst < ${BASE_PATH}/deploy/configuration/api/${API_IMAGE}.yaml)
+        kubectl apply --template="'"${template}"'"
     done
 }
 
@@ -35,7 +36,8 @@ function delete_api_application(){
     for ix in ${!STORAGES[*]}
     do
         API_IMAGE="iot-api-"${STORAGES[$ix]}
-        kubectl delete -f ${BASE_PATH}/deploy/configuration/api/${API_IMAGE}.yaml
+        template=$(envsubst < ${BASE_PATH}/deploy/configuration/api/${API_IMAGE}.yaml)
+        kubectl delete --template="'"${template}"'"
     done
 }
 
@@ -44,8 +46,8 @@ function deploy_storage_image(){
     do
         API_IMAGE="iot-storage-"${STORAGES[$ix]}
         docker build -f ${BASE_PATH}/deploy/configuration/storage/docker-images/Dockerfile-${API_IMAGE} -t ${API_IMAGE}:${VERSION} .
-        docker tag ${API_IMAGE}:${VERSION} ${REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
-        docker push ${REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
+        docker tag ${API_IMAGE}:${VERSION} ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
+        docker push ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
     done
 }
 
@@ -53,7 +55,8 @@ function deploy_storage_application(){
     for ix in ${!STORAGES[*]}
     do
         API_IMAGE="iot-storage-"${STORAGES[$ix]}
-        kubectl apply -f ${BASE_PATH}/deploy/configuration/storage/${API_IMAGE}.yaml
+        template=$(envsubst < ${BASE_PATH}/deploy/configuration/storage/${API_IMAGE}.yaml)
+        kubectl apply --template="'"${template}"'"
     done
 }
 
@@ -61,34 +64,40 @@ function delete_storage_application(){
     for ix in ${!STORAGES[*]}
     do
         API_IMAGE="iot-storage-"${STORAGES[$ix]}
-        kubectl delete -f ${BASE_PATH}/deploy/configuration/storage/${API_IMAGE}.yaml
+        template=$(envsubst < ${BASE_PATH}/deploy/configuration/storage/${API_IMAGE}.yaml)
+        kubectl delete --template="'"${template}"'"
     done
 }
 
 function deploy_connector_image(){
     docker build -f ${BASE_PATH}/deploy/configuration/connector/docker-images/Dockerfile-${CONNECTOR_IMAGE} -t ${CONNECTOR_IMAGE}:${VERSION} .
-    docker tag ${CONNECTOR_IMAGE}:${VERSION} ${REPOSITORY}/${PROJECT_NAME}/${CONNECTOR_IMAGE}:${VERSION}
-    docker push ${REPOSITORY}/${PROJECT_NAME}/${CONNECTOR_IMAGE}:${VERSION}
+    docker tag ${CONNECTOR_IMAGE}:${VERSION} ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${CONNECTOR_IMAGE}:${VERSION}
+    docker push ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${CONNECTOR_IMAGE}:${VERSION}
 }
 
 function deploy_connector_application(){
-    kubectl apply -f ${BASE_PATH}/deploy/configuration/connector/${CONNECTOR_IMAGE}.yaml
+    kubectl apply -f
+    template=$(envsubst < ${BASE_PATH}/deploy/configuration/connector/${CONNECTOR_IMAGE}.yaml)
+    kubectl apply --template="'"${template}"'"
 }
 
 function delete_connector_application(){
-    kubectl delete -f ${BASE_PATH}/deploy/configuration/connector/${CONNECTOR_IMAGE}.yaml
+    template=$(envsubst < ${BASE_PATH}/deploy/configuration/connector/${CONNECTOR_IMAGE}.yaml)
+    kubectl delete --template="'"${template}"'"
 }
 
 function deploy_notification_image(){
     docker build -f ${BASE_PATH}/deploy/configuration/notification/docker-images/Dockerfile-${NOTIFICATION_IMAGE} -t ${NOTIFICATION_IMAGE}:${VERSION} .
-    docker tag ${NOTIFICATION_IMAGE}:${VERSION} ${REPOSITORY}/${PROJECT_NAME}/${NOTIFICATION_IMAGE}:${VERSION}
-    docker push ${REPOSITORY}/${PROJECT_NAME}/${NOTIFICATION_IMAGE}:${VERSION}
+    docker tag ${NOTIFICATION_IMAGE}:${VERSION} ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${NOTIFICATION_IMAGE}:${VERSION}
+    docker push ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${NOTIFICATION_IMAGE}:${VERSION}
 }
 
 function deploy_notification_application(){
-    kubectl apply -f ${BASE_PATH}/deploy/configuration/notification/${NOTIFICATION_IMAGE}.yaml
+    template=$(envsubst < ${BASE_PATH}/deploy/configuration/notification/${NOTIFICATION_IMAGE}.yaml)
+    kubectl apply --template="'"${template}"'"
 }
 
 function delete_notification_application(){
-    kubectl delete -f ${BASE_PATH}/deploy/configuration/notification/${NOTIFICATION_IMAGE}.yaml
+    template=$(envsubst < ${BASE_PATH}/deploy/configuration/notification/${NOTIFICATION_IMAGE}.yaml)
+    kubectl delete --template="'"${template}"'"
 }

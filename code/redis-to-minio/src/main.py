e0506fc7af875f07d3c626dc736b3072098b90a6
sennoun <ysennoun@xebia.fr>
2019-10-20 16:55:47 +0200

    [ADD][dev] delete file after uploading to minIO

--- code/redis-to-minio/src/main.py
+++ code/redis-to-minio/src/main.py
@@ -1,22 +1,26 @@
+import os
 from common import common_redis, common_minio
 from consume import consumer
 from sink import sinker
 
 
-class SinkerMain:
-    def run(self):
-        redis_client = common_redis.get_redis_client()
-        minio_client = common_minio.get_minio_client()
+def delete_file(file_path: str):
+    os.system(f"rm -f {file_path}")
 
-        file_path = consumer.get_messages_stored_in_a_file(redis_client)
-        sinker.create_bucket(minio_client)
-        sinker.upload_object(minio_client, file_path)
+
+def run():
+    redis_client = common_redis.get_redis_client()
+    minio_client = common_minio.get_minio_client()
+
+    file_path = consumer.get_messages_stored_in_a_file(redis_client)
+    sinker.create_bucket(minio_client)
+    sinker.upload_object(minio_client, file_path)
+    delete_file(file_path)
 
 
 if __name__ == "__main__":
     try:
-        sinker_main = SinkerMain()
         while True:
-            sinker_main.run()
+            run()
     except Exception as ex:
         print(f"Mqtt client failed: {str(ex)}")

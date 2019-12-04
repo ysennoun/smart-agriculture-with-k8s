11a9a28b0c6adebead5f5e78cbd30da4278f2141
sennoun <ysennoun@xebia.fr>
2019-12-04 07:57:46 +0100

    [ADD][dev] reproduce the same hierarchy in deploy compared to code

--- deploy/code/deploy_code.sh
+++ deploy/code/deploy_code.sh
@@ -22,7 +22,7 @@ function deploy_api_image(){
     for ix in ${!STORAGES[*]}
     do
         API_IMAGE="api-"${STORAGES[$ix]}
-        docker build -f ${BASE_PATH}/deploy/code/api/docker-images/Dockerfile-${API_IMAGE} -t ${API_IMAGE}:${VERSION} .
+        docker build -f ${BASE_PATH}/deploy/code/serverless/api/docker-images/Dockerfile-${API_IMAGE} -t ${API_IMAGE}:${VERSION} .
         docker tag ${API_IMAGE}:${VERSION} ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
         docker push ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
     done
@@ -32,7 +32,7 @@ function deploy_api_application(){
     for ix in ${!STORAGES[*]}
     do
         API_IMAGE="api-"${STORAGES[$ix]}
-        template=$(get_template ${BASE_PATH}/deploy/code/api/${API_IMAGE}.yaml)
+        template=$(get_template ${BASE_PATH}/deploy/code/serverless/api/${API_IMAGE}.yaml)
         kubectl apply --template="'"${template}"'"
     done
 }
@@ -41,7 +41,7 @@ function delete_api_application(){
     for ix in ${!STORAGES[*]}
     do
         API_IMAGE="api-"${STORAGES[$ix]}
-        template=$(get_template ${BASE_PATH}/deploy/code/api/${API_IMAGE}.yaml)
+        template=$(get_template ${BASE_PATH}/deploy/code/serverless/api/${API_IMAGE}.yaml)
         kubectl delete --template="'"${template}"'"
     done
 }
@@ -50,7 +50,7 @@ function deploy_storage_image(){
     for ix in ${!STORAGES[*]}
     do
         API_IMAGE="storage-"${STORAGES[$ix]}
-        docker build -f ${BASE_PATH}/deploy/code/storage/docker-images/Dockerfile-${API_IMAGE} -t ${API_IMAGE}:${VERSION} .
+        docker build -f ${BASE_PATH}/deploy/code/serverless/storage/docker-images/Dockerfile-${API_IMAGE} -t ${API_IMAGE}:${VERSION} .
         docker tag ${API_IMAGE}:${VERSION} ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
         docker push ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${API_IMAGE}:${VERSION}
     done
@@ -60,7 +60,7 @@ function deploy_storage_application(){
     for ix in ${!STORAGES[*]}
     do
         API_IMAGE="storage-"${STORAGES[$ix]}
-        template=$(get_template ${BASE_PATH}/deploy/code/storage/${API_IMAGE}.yaml)
+        template=$(get_template ${BASE_PATH}/deploy/code/serverless/storage/${API_IMAGE}.yaml)
         kubectl apply --template="'"${template}"'"
     done
 }
@@ -69,40 +69,40 @@ function delete_storage_application(){
     for ix in ${!STORAGES[*]}
     do
         API_IMAGE="storage-"${STORAGES[$ix]}
-        template=$(get_template ${BASE_PATH}/deploy/code/storage/${API_IMAGE}.yaml)
+        template=$(get_template ${BASE_PATH}/deploy/code/serverless/storage/${API_IMAGE}.yaml)
         kubectl delete --template="'"${template}"'"
     done
 }
 
 function deploy_mqtt_image(){
-    docker build -f ${BASE_PATH}/deploy/code/mqtt/docker-images/Dockerfile-${MQTT_IMAGE} -t ${MQTT_IMAGE}:${VERSION} .
+    docker build -f ${BASE_PATH}/deploy/code/serverless/mqtt/docker-images/Dockerfile-${MQTT_IMAGE} -t ${MQTT_IMAGE}:${VERSION} .
     docker tag ${MQTT_IMAGE}:${VERSION} ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${MQTT_IMAGE}:${VERSION}
     docker push ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${MQTT_IMAGE}:${VERSION}
 }
 
 function deploy_mqtt_consumer(){
     kubectl apply -f
-    template=$(get_template ${BASE_PATH}/deploy/code/mqtt/${MQTT_IMAGE}.yaml)
+    template=$(get_template ${BASE_PATH}/deploy/code/serverless/mqtt/${MQTT_IMAGE}.yaml)
     kubectl apply --template="'"${template}"'"
 }
 
 function delete_mqtt_consumer(){
-    template=$(get_template ${BASE_PATH}/deploy/code/mqtt/${MQTT_IMAGE}.yaml)
+    template=$(get_template ${BASE_PATH}/deploy/code/serverless/mqtt/${MQTT_IMAGE}.yaml)
     kubectl delete --template="'"${template}"'"
 }
 
 function deploy_notification_image(){
-    docker build -f ${BASE_PATH}/deploy/code/notification/docker-images/Dockerfile-${NOTIFICATION_IMAGE} -t ${NOTIFICATION_IMAGE}:${VERSION} .
+    docker build -f ${BASE_PATH}/deploy/code/serverless/notification/docker-images/Dockerfile-${NOTIFICATION_IMAGE} -t ${NOTIFICATION_IMAGE}:${VERSION} .
     docker tag ${NOTIFICATION_IMAGE}:${VERSION} ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${NOTIFICATION_IMAGE}:${VERSION}
     docker push ${CONTAINER_REPOSITORY}/${PROJECT_NAME}/${NOTIFICATION_IMAGE}:${VERSION}
 }
 
 function deploy_notification_application(){
-    template=$(get_template ${BASE_PATH}/deploy/code/notification/${NOTIFICATION_IMAGE}.yaml)
+    template=$(get_template ${BASE_PATH}/deploy/code/serverless/notification/${NOTIFICATION_IMAGE}.yaml)
     kubectl apply --template="'"${template}"'"
 }
 
 function delete_notification_application(){
-    template=$(get_template ${BASE_PATH}/deploy/code/notification/${NOTIFICATION_IMAGE}.yaml)
+    template=$(get_template ${BASE_PATH}/deploy/code/serverless/notification/${NOTIFICATION_IMAGE}.yaml)
     kubectl delete --template="'"${template}"'"
 }

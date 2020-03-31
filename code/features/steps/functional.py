f1d6f4701916f948c2e559428af8ec7f367e645a
Yassir Sennoun <yassir.sennoun-ext@sanofi.com>
2020-03-31 09:57:50 +0200

    Add print

--- code/features/steps/functional.py
+++ code/features/steps/functional.py
@@ -8,6 +8,7 @@ from steps import utils
 @given('An IoT message is sent to the platform for device {device} with temperature '
        '{temperature} in topic {topic}')
 def step_impl(context, device, temperature, topic):
+    print("given mqtt")
     iot_data = json.dumps({
         "device": device,
         "timestamp": utils.get_current_timestamp(),
@@ -16,24 +17,30 @@ def step_impl(context, device, temperature, topic):
         "moisture": 10
     })
     mqtt_pod_name = f"mqtt-pod-{randint(0, 10000)}"
+    print(f"mqtt_pod_name : {mqtt_pod_name}")
     core_v1 = utils.get_core_v1()
     mqtt_pod_manifest = utils.get_mqtt_pod_manifest(mqtt_pod_name, iot_data, topic)
+    print(f"mqtt_pod_manifest : {mqtt_pod_manifest}")
     utils.run_pod(core_v1, mqtt_pod_name, mqtt_pod_manifest)
+    print("delete pod")
     utils.delete_pod(core_v1, mqtt_pod_name)
 
 
 @given('wait {min} min to let system handle data')
 def step_impl(context, min):
     time_to_sleep = int(min) * 60
+    print(f"time_to_sleep : {time_to_sleep}")
     time.sleep(time_to_sleep)
 
 
 @then('For device {device}, the temperature of the last value should be equal to {temperature}')
 def step_impl(context, device, temperature):
+    print(f"then last value")
     core_v1 = utils.get_core_v1()
     back_end_pod_name = f"back-end-pod-{randint(0, 10000)}"
     back_end_pod_manifest = utils.get_back_end_pod_manifest(back_end_pod_name, f"/device/last-value/{device}")
     result = utils.run_pod(core_v1, back_end_pod_name, back_end_pod_manifest)
+    print(f"result: {result}")
     utils.delete_pod(core_v1, back_end_pod_name)
     assert json.loads(result)["rows"][0]["temperature"] is temperature
 
@@ -41,11 +48,13 @@ def step_impl(context, device, temperature):
 @then('For device {device}, timeseries should contain {number_of_elements} elements '
       'and temperatures should be in')
 def step_impl(context, device, number_of_elements):
+    print(f"then timeseries")
     core_v1 = utils.get_core_v1()
     back_end_pod_name = f"back-end-pod-{randint(0, 10000)}"
     back_end_pod_manifest = utils.get_back_end_pod_manifest(back_end_pod_name, f"/device/timeseries/{device}")
     result = utils.run_pod(core_v1, back_end_pod_name, back_end_pod_manifest)
     utils.delete_pod(core_v1, back_end_pod_name)
+    print(f"result: {result}")
     timeseries = json.loads(result)["rows"]
     temperatures = [element["temperature"] for element in timeseries]
     assert number_of_elements is len(timeseries)

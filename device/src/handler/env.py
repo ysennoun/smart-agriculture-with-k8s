d9e4681143ba72209c7d679512972efb59b4b0fb
Yassir Sennoun <yassir.sennoun-ext@sanofi.com>
2020-06-30 21:16:54 +0200

    solve env problem in device code

--- device/src/handler/env.py
+++ device/src/handler/env.py
@@ -2,11 +2,11 @@ import os
 
 
 def get_publishing_frequency():
-    return int(os.environ.environ['PUBLISHING_FREQUENCY'])  # number of minutes
+    return int(os.environ['PUBLISHING_FREQUENCY'])  # number of minutes
 
 
 def get_mqtt_host_ip():
-    return os.environ.environ['MQTT_HOST_IP']
+    return os.environ['MQTT_HOST_IP']
 
 
 def get_mqtt_host_port():
@@ -18,7 +18,7 @@ def get_mqtt_topic():
 
 
 def get_keep_alive():
-    return int(os.getenv("KEEP_ALIVE", 60))  # in seconds
+    return int(os.environ.get("KEEP_ALIVE", 60))  # in seconds
 
 
 def get_mqtt_client_id():

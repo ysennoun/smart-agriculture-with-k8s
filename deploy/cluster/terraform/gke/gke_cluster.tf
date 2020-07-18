691c974c69e938aebc27c9464f75e1e27d667b68
Yassir Sennoun <yassir.sennoun-ext@sanofi.com>
2020-07-18 19:02:10 +0200

    update terraform gke template

--- deploy/cluster/terraform/gke/gke_cluster.tf
+++ deploy/cluster/terraform/gke/gke_cluster.tf
@@ -83,18 +83,13 @@ resource "google_container_node_pool" "np" {
 
 data "google_client_config" "provider" {}
 
-data "google_container_cluster" "k8s_cluster" {
-  name     = var.cluster_name
-  location = var.zone
-}
-
 provider "kubernetes" {
   load_config_file = false
 
-  host  = "https://${data.google_container_cluster.k8s_cluster.endpoint}"
+  host  = "https://${google_container_cluster.primary.endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.k8s_cluster.master_auth[0].cluster_ca_certificate,
+    google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
   )
 }
 
